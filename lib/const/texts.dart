a2cabeb0980935ba0010cadda23e056888c99b57
Sandra-chemist <sandra.skibiszewska.programista@gmail.com>
2024-12-14 17:47:08 +0100

    fix routes

--- lib/const/texts.dart
+++ lib/const/texts.dart
@@ -18,7 +18,7 @@ const String searchUrl = 'https://fuksiarz.pl/rest/search/events/quick-search';
 const String loremIpsumText = """
 # 1. Organizer  
 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.
-# 
+# git push
 #
 # 1. Organizer    
 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.
--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -8,8 +8,8 @@ class AppRouter extends RootStackRouter {
 
   @override
   List<AutoRoute> get routes => [
-        // AutoRoute(page: SplashRoute.page, initial: true),
-        AutoRoute(page: HomeRoute.page, initial: true),
+        AutoRoute(page: SplashRoute.page, initial: true),
+        AutoRoute(page: HomeRoute.page),
         AutoRoute(page: SearchRoute.page),
       ];
 }
